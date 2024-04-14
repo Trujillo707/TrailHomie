da83157fe026d32c587db756ce5bcc0b7fecf168
Dylan-Westigard <163870868+Dylan-Westigard@users.noreply.github.com>
2024-04-14 04:39:39 -0700

    AYYYY WE GETTIN OPALCES

--- app/src/main/java/com/percolators/trailhomie/ReportScreen.kt
+++ app/src/main/java/com/percolators/trailhomie/ReportScreen.kt
@@ -1,15 +1,9 @@
 package com.percolators.trailhomie
-
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -18,32 +12,47 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import android.os.CountDownTimer
+import androidx.compose.runtime.mutableIntStateOf
 
+
+var end = false
+object Timer: CountDownTimer(30000, 1000) {
+
+    override fun onTick(millisUntilFinished: Long) {
+    }
+
+    override fun onFinish() {
+         end = true
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ReportScreen(trailToReport:String, navController: NavController) {
     val theTrail = TrailList.searchByName(trailToReport)
-
+    var color by remember { mutableIntStateOf( -1) }
 
     Column(
         modifier = Modifier
             .fillMaxHeight()
-            .background(color = Color(red = 212, green = 169, blue = 219 ))
+            .background(color = Color(red = 212, green = 169, blue = 219))
 
     ) {
 
-        Row(modifier = Modifier.weight(1.75f)) {
+        Row(modifier = Modifier
+            .align(alignment = Alignment.CenterHorizontally)
+            .weight(1.75f)) {
             Text(modifier = Modifier,
                     text = trailToReport,
                     fontSize = 64.sp
@@ -87,34 +96,95 @@ fun ReportScreen(trailToReport:String, navController: NavController) {
             .weight(1f)
             .align(alignment = Alignment.CenterHorizontally)
             ) {
+            ElevatedCard(modifier = Modifier
+                .weight(1f),
+                colors = if (color == 1) {
+                        CardDefaults.cardColors(containerColor = Color.White)
+                }else {
+                   CardDefaults.cardColors(containerColor = Color.Gray)
+                },
+                shape = CircleShape,
+                onClick = {
+                    while (!end) {
+                        if (color == -1) {
+                            Timer.start()
+                        }
+                        if (color != 1) {
+                            color = 1
+                            break
+                        }
+                    }
+                }
+            )
+            {
+                Image(
+                    painter = painterResource(R.drawable.test_yo_mama),
+                    contentDescription = "trail Image",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .align(alignment = Alignment.CenterHorizontally)
+                )
+            }
+            ElevatedCard(modifier = Modifier
+                .weight(1f),
+                colors = if (color == 2) {
+                    CardDefaults.cardColors(containerColor = Color.White)
+                }else {
+                    CardDefaults.cardColors(containerColor = Color.Gray)
+                },
+                shape = CircleShape,
+                onClick = {
+                    while (!end) {
+                        if (color == -1) {
+                            Timer.start()
+                        }
+                        if (color != 2) {
+                            color = 2
+                            break
+                        }
+                    }
+                }
+            )
+            {
+                Image(
+                    painter = painterResource(R.drawable.test_yo_mama),
+                    contentDescription = "trail Image",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .align(alignment = Alignment.CenterHorizontally)
+                )
+            }
+            ElevatedCard(modifier = Modifier
+                .weight(1f),
+                colors = if (color == 3) {
+                    CardDefaults.cardColors(containerColor = Color.White)
+                }else {
+                    CardDefaults.cardColors(containerColor = Color.Gray)
+                },
+                shape = CircleShape,
+                onClick = {
+                    while (!end) {
+                        if (color == -1) {
+                            Timer.start()
+                        }
+                        if (color != 3) {
+                            color = 3
+                            break
+                        }
+                    }
+                }
+            )
+            {
+                Image(
+                    painter = painterResource(R.drawable.test_yo_mama),
+                    contentDescription = "trail Image",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .align(alignment = Alignment.CenterHorizontally)
 
-            Spacer(modifier = Modifier.weight(0.29f))
-            Column(modifier = Modifier
-                .weight(1f)) {
-                Image(
-                    painter = painterResource(R.drawable.test_yo_mama),
-                    contentDescription = "trail Image",
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .clip(CircleShape)
-                )
-            }
-            Column(modifier = Modifier.weight(1f)) {
-                Image(
-                    painter = painterResource(R.drawable.test_yo_mama),
-                    contentDescription = "trail Image",
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .clip(CircleShape)
-                )
-            }
-            Column(modifier = Modifier.weight(1f)) {
-                Image(
-                    painter = painterResource(R.drawable.test_yo_mama),
-                    contentDescription = "trail Image",
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .clip(CircleShape)
                     )
                 }
 
