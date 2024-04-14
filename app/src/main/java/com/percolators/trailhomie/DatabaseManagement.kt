8e21e8c282a22a16acede1aef545212e6860dcf1
Orlando Trujillo-Ortiz <orlandotrujillo573@gmail.com>
2024-04-14 04:40:43 -0700

    database stuff

--- app/src/main/java/com/percolators/trailhomie/DatabaseManagement.kt
+++ app/src/main/java/com/percolators/trailhomie/DatabaseManagement.kt
@@ -1,14 +1,21 @@
 package com.percolators.trailhomie
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.MainThread
 import androidx.compose.runtime.Composable
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
-class DatabaseManagement{
-    val db = Firebase.firestore
+object DatabaseManagement{
 
     /**
         Get a trail name and new reported condition -> Make a DB reports
@@ -16,28 +23,32 @@ class DatabaseManagement{
         @param trailName The name of the trail in question
         @param newCondition The new condition
      */
-    fun sendReport(trailName:String, newCondition:Int){
-
+    fun sendReport(trailName:String, newCondition:Int) = CoroutineScope(Dispatchers.IO).launch{
+        val searchResult = Firebase.firestore.collection("humboldt_trails").whereEqualTo("name", trailName).get().await()
+        for (currDoc in searchResult){
+            //val theSubmission = Map<>
+            //Firebase.firestore.collection("humboldt_reports").add()
+        }
     }
 
     /**
-     * Get a map of <TrailName:String, TrailCondition:Int>. Probably use for
+     * Get a the documents in the humboldt_trails collection Probably use for
      * creating the list of trails later.
      */
-//    fun getTrailInfo():Map<String, Int>{
-//        var trails = mutableMapOf<String, Int>()
-//        db.collection("humboldtTrails")
-//            .get()
-//            .addOnSuccessListener { result ->
-//                for (document in result) {
-//                    document.data
-//                    //trails.put(document.get("name", String), document.get("content"))
-//                }
-//            }
-//            .addOnFailureListener { exception ->
-//                Log.e("DatabaseManagement::getTrailInfo()","Cannot access humboldtTrails", exception)
-//            }
-//    }
+     fun getTrailInfo() = CoroutineScope(Dispatchers.IO).launch {
+        val result = Firebase.firestore.collection("humboldt_trails")
+            .get()
+            .await()
+        for (document in result) {
+            Log.d(ContentValues.TAG, "${document.id} => ${document.data}")
+            val theName = document.get("name")
+            val theCondition = document.get("condition")
+            val tempTrail = Trail(theName.toString(),theCondition as Long)
+            TrailList.addTrail(tempTrail)
+            Log.d(ContentValues.TAG,"The size here is ${TrailList.getSize()}")
+        }
+    }
+
 
     fun updateCondition(){
 
